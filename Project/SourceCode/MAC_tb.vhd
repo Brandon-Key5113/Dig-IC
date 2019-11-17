ff19486229c45055e87601ce14129d49657f51e6
cdg6285 <cdg6285@cs.rit.edu>
2019-11-17 14:11:13 -0500

    Adding changes to wrapper and test bench

--- Project/SourceCode/MAC_tb.vhd
+++ Project/SourceCode/MAC_tb.vhd
@@ -11,19 +11,21 @@ entity MAC_tb is
 end;
 
 architecture bench of MAC_tb is
+  
+  constant N : integer := 32;
 
   component MAC
-  	 generic( N : integer :=16);
-      Port ( A : in  STD_LOGIC_VECTOR (N-1 downto 0);
-             B : in  STD_LOGIC_VECTOR (N-1 downto 0);
-             clk : in STD_LOGIC;
-             WE : in STD_LOGIC;
-             reset : in STD_LOGIC;
-             RegOut : out  STD_LOGIC_VECTOR (N-1 downto 0));
+  	 generic( N : integer := 32);
+      Port ( A : in  STD_LOGIC_VECTOR ((N/2) - 1  downto 0);
+           B : in  STD_LOGIC_VECTOR ((N/2) - 1 downto 0);
+           clk : in STD_LOGIC;
+           WE : in STD_LOGIC;
+           reset : in STD_LOGIC;
+           RegOut : out  STD_LOGIC_VECTOR (N-1 downto 0));
   end component;
 
-  signal A: STD_LOGIC_VECTOR (N-1 downto 0);
-  signal B: STD_LOGIC_VECTOR (N-1 downto 0);
+  signal A: STD_LOGIC_VECTOR ((N/2) - 1  downto 0);
+  signal B: STD_LOGIC_VECTOR ((N/2) - 1 downto 0);
   signal clk: STD_LOGIC;
   signal WE: STD_LOGIC;
   signal reset: STD_LOGIC;
@@ -32,19 +34,33 @@ architecture bench of MAC_tb is
 begin
 
   -- Insert values for generic parameters !!
-  uut: MAC generic map ( N      =>  )
+  uut: MAC generic map ( N      => 32)
               port map ( A      => A,
                          B      => B,
                          clk    => clk,
                          WE     => WE,
                          reset  => reset,
                          RegOut => RegOut );
+								 
+	clk_proc : process
+	begin
+		if clk = '0' then
+			clk <= '1';
+		else 
+			clk <= '0';
+		end if;
+		wait for 50 ns;
+	end process;
 
   stimulus: process
   begin
   
     -- Put initialisation code here
-
+	 WE <= '1';
+	 reset <= '0';
+	A <= "0000000000000010";
+	B <= "0000000000000010";
+	 
 
     -- Put test bench stimulus code here
 
