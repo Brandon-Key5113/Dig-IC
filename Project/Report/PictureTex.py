5bb05831a8283885bd30511ca3099abc95823224
Brandon-Key5113 <bxk5113@rit.edu>
2019-12-04 17:07:13 -0500

    Added Pictures to report

--- Project/Report/PictureTex.py
+++ Project/Report/PictureTex.py
@@ -1,9 +1,11 @@
-formaty = "\\begin{figure}[H] \n\
+formaty1 = "\\begin{figure}[H] \n\
     \\centering \n\
-    \\includegraphics[width=1\linewidth]{\""
-formaty2= "\"}\n\
-    \\caption{Functional Simulation of 1-bit ALU} \n\
-    \\label{fig:alu1bit-functional-sim-} \n\
+    \\includegraphics[width=0.7\linewidth]{\""
+formaty2 = "\"}\n\
+    \\caption{"
+formaty3 = "} \n\
+    \\label{fig:"
+formaty4 = "} \n\
 \\end{figure}\n"
 
 #TODO Add Smart caption and label
@@ -12,11 +14,21 @@ import os
 
 files = []
 
+
+def rchop(thestring, ending):
+  if thestring.lower().endswith(ending):
+    return thestring[:-len(ending)]
+  return thestring
+
 for file in os.listdir("./Pictures"):
-    if file.endswith(".png"):
+    if file.lower().endswith(".png"):
         files += [file]
 
 
 for filez in files:
-    print (formaty + filez + formaty2 + "\n")
-print(files)
+    noEnd = rchop(filez, ".png")
+    toDash = noEnd.replace(' ', '-').replace('_', '-')
+    toUnd  = noEnd.replace(' ', '_').replace('-', '_')
+    toSp   = noEnd.replace('_', ' ').replace('-', ' ')
+
+    print (formaty1 + "Pictures/" + noEnd + formaty2 + toSp + formaty3 + toDash + formaty4 + "\n")
